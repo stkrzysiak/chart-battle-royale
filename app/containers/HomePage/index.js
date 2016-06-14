3ae6701e1187f289ce3d7c0c415a3d6e1351a8ce
admin <server@servers-MacBook-Pro.local>
2016-06-13 19:24:04 -0500

    Basic chart works now, baby steps.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,41 +10,8 @@
  */
 
 import React from 'react';
-import NVD3Chart from 'react-nvd3';
-
-function getDatum(j) {
-  const sin = [];
-  const cos = [];
-
-  for (let i = 0; i < 100; i++) {
-    sin.push({ x: i, y: Math.sin(i / j) });
-    cos.push({ x: i, y: 0.5 * Math.cos(i / j) });
-  }
-
-  return [
-    {
-      values: sin,
-      key: 'Sine Wave',
-      color: '#ff7f0e',
-    },
-    {
-      values: cos,
-      key: 'Cosine Wave',
-      color: '#2ca02c',
-    },
-  ];
-}
+import LineChartNvd3 from '../../components/LineChartNvd3';
 
 
-export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
-
-  render() {
-    const datum = getDatum();
-    return (
-      <div>
-        <h1> Sample chart</h1>
-        <NVD3Chart id="barChart" type="discreteBarChart" datum={datum} x="label" y="value" />
-      </div>
-    );
-  }
-}
+export default () =>
+  (<LineChartNvd3 />);
